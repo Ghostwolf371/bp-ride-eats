d561e0e723c2077a886b577a378d943c684ce787
ghostwolf <terrencelinger05@gmail.com>
2025-07-25 18:05:54 -0300

    testing

--- frontend/src/components/Orders.vue
+++ frontend/src/components/Orders.vue
@@ -108,7 +108,10 @@
             <HomeIcon class="h-4 w-4 mr-2 text-green-400" />
             <span class="text-sm truncate">{{ order.deliveryAddress }}</span>
           </div>
-          <div class="flex items-center text-gray-300">
+          <div
+            v-if="order.status === 'COMPLETED'"
+            class="flex items-center text-gray-300"
+          >
             <BikeIcon class="h-4 w-4 mr-2 text-green-400" />
             <span class="text-sm truncate">{{ order.assignedBiker }}</span>
           </div>
